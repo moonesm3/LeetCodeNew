396d4e094c832c9a5d7a0f5f520aa8be1fe768b6
Moones Mobaraki <moonesmobaraki@Mac.home>
2026-06-01 21:59:34 +0200

    First Problem

---
+++ 2144.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumCost(self, cost: list[int]) -> int:
+        cost.sort(reverse=True)
+        total = 0
+        for i in range(len(cost)):
+            if i < 2:
+                total += cost[i]
+            elif i % 3 == 0:
+                total += cost[i]
+            elif i % 3 == 1:
+                total += cost[i]
+        return total
+    
+my_solution = Solution()
+print(my_solution.minimumCost([1, 2, 3])) # 5
+print(my_solution.minimumCost([6,5,7,9,2,2])) 
+print(my_solution.minimumCost([5,5])) # 10
