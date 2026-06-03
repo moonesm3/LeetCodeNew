00c7f45206fb20cf5751f5d5a0ce56e03ac76db1
MoonesMobaraki <moones.mobaraki@m31.com>
2026-06-03 15:49:54 +0200

    11th Problem

---
+++ 35.py
@@ -0,0 +1,14 @@
+# 35. Search Insert Position
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        left, right = 0, len(nums)
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+my_solution = Solution()
+print(my_solution.searchInsert([1,3,5,6], 5))  # Output: 2
+print(my_solution.searchInsert([1,3,5,6], 2))  # Output: 1
