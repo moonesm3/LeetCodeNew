8d15a6fa9ec7a0a29375cc64fb4cf4b4b161ffc3
MoonesMobaraki <moones.mobaraki@m31.com>
2026-06-04 11:51:44 +0200

    15th Problem

---
+++ 69.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        left, right = 1, x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
+my_solution = Solution()
+print(my_solution.mySqrt(4)) # 2
+print(my_solution.mySqrt(8)) # 2
