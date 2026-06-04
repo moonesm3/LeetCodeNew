16b4468d7a817871082979677d2e5f40f6875cde
MoonesMobaraki <moones.mobaraki@m31.com>
2026-06-04 09:16:27 +0200

    12th Problem

---
+++ 58.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.split()
+        print(f"Words: {words}")
+        if words[-1] == '':
+            return len(words[-2])
+        return len(words[-1])
+    
+my_solution = Solution()
+print(my_solution.lengthOfLastWord("Hello World"))  # Output: 5
+print(my_solution.lengthOfLastWord("   fly me   to   the moon  ")) # Output: 4
+print(my_solution.lengthOfLastWord("luffy is still joyboy")) # Output: 6
