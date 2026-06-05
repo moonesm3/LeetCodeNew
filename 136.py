efdb96951c8f62055fc6f57c8ca1fa619f466cdb
Moones Mobaraki <moonesmobaraki@Mac.home>
2026-06-05 12:40:29 +0200

    31th Problem

---
+++ 136.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        for num in nums:
+            if nums.count(num) == 1:
+                return num
+my_solution = Solution()
+print(my_solution.singleNumber([2, 2, 1]))  # Output: 1
+print(my_solution.singleNumber([4, 1, 2, 1, 2]))  # Output: 4
