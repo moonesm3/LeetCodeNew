3f50d9a0749e79ccbcbc9d7a99eb6c91e9ef949e
Moones Mobaraki <moonesmobaraki@Mac.home>
2026-06-06 00:31:34 +0200

    32th Problem

--- 141.py
+++ 141.py
@@ -1,3 +1,4 @@
+# 141. Linked List Cycle
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
@@ -11,14 +12,15 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if not head:
             return False
-        slow = head
-        fast = head
-        while fast and fast.next:
+        slow = head     
+        fast = head.next
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
             slow = slow.next
             fast = fast.next.next
-            if slow == fast:
-                return True
-        return False
+        return True
+
     
 my_solution = Solution()
 print(my_solution.hasCycle([3,2,0,-4]))  # Output: True
