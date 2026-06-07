434b7201ac1252dfcbc1f83f9cc24819095589e1
Moones Mobaraki <moonesmobaraki@Mac.home>
2026-06-07 19:52:20 +0200

    42th Problem

---
+++ 190.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+        for _ in range(32):
+            result = (result << 1) | (n & 1)
+            n >>= 1
+        return result
+
+
+my_solution = Solution()
+print(my_solution.reverseBits(43261596))
