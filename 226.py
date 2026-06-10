6e63305d1a2eaa6d38868cb8c218b5ebec373bca
Moones Mobaraki <moonesmobaraki@Mac.home>
2026-06-10 19:47:59 +0200

    56th Problem

---
+++ 226.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root):
+        if root is None:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+    
+my_solution = Solution()
+print(my_solution.invertTree(root = [4,2,7,1,3,6,9]))
