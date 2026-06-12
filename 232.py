91aa982097670e79e33b109d617c81c36991dfbc
MoonesMobaraki <moones.mobaraki@m31.com>
2026-06-12 14:48:00 +0200

    59th Problem

---
+++ 232.py
@@ -0,0 +1,32 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack_in = []
+        self.stack_out = []
+
+    def push(self, x: int) -> None:
+        self.stack_in.append(x)
+        
+    def pop(self) -> int:
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
+        return self.stack_out.pop()
+        
+    def peek(self) -> int:
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
+        return self.stack_out[-1]
+        
+    def empty(self) -> bool:
+        return len(self.stack_in) == 0 and len(self.stack_out) == 0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
