48f096298fe3d32e1250f43883bfa1ad224bb998
MoonesMobaraki <moones.mobaraki@m31.com>
2026-06-16 14:18:16 +0200

    70th Problem

---
+++ 303.py
@@ -0,0 +1,16 @@
+class NumArray:
+
+    def __init__(self, nums: list[int]):
+        self.prefix = [0]
+        for num in nums:
+            self.prefix.append(self.prefix[-1] + num)
+            
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix[right + 1] - self.prefix[left]
+
+
+obj = NumArray([-2, 0, 3, -5, 2, -1])
+
+print(obj.sumRange(0, 2))  # 1
+print(obj.sumRange(2, 5))  # -1
+print(obj.sumRange(0, 5))  # -3
