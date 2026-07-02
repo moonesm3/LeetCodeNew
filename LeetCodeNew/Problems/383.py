334c1b24c73b40c6951b82eb88478325e99046f7
MoonesMobaraki <moones.mobaraki@m31.com>
2026-07-02 14:50:44 +0200

    80th Problem

---
+++ LeetCodeNew/Problems/383.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for char in ransomNote:
+            if char not in magazine:
+                return False
+            magazine = magazine.replace(char, "", 1)
+        return True
+    
+my_solution = Solution()
+print(my_solution.canConstruct(ransomNote = "a", magazine = "b"))    #Output: False
+print(my_solution.canConstruct(ransomNote = "aaa", magazine = "baaa"))    #Output: True
+print(my_solution.canConstruct(ransomNote = "a", magazine = "ab"))    #Output: False
