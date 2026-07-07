6133a4666e070b7a7c8d09771d4ce15c4dbc3f0e
MoonesMobaraki <moones.mobaraki@m31.com>
2026-07-07 12:58:16 +0200

    96th problem

---
+++ LeetCodeNew/Problems/476.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        binary = bin(num)[2:]
+        complement = ""
+        for i in range(len(binary)):
+            if binary[i] == "0":
+                complement += "1"
+            else:
+                complement += "0"
+        return int(complement, 2)
+        
+        
+        
+my_solution = Solution()
+print(my_solution.findComplement(5))      #Output: 2
