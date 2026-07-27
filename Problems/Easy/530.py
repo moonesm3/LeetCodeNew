9b4af79f84771efdcb657304f305720a0de5af4f
MoonesMobaraki <moones.mobaraki@m31.com>
2026-07-27 15:50:42 +0200

    Easy

---
+++ Problems/Easy/530.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        values = []
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            values.append(node.val)
+            inorder(node.right)
+
+        inorder(root)
+        minimum = float("inf")
+        for i in range(1, len(values)):
+            difference = values[i] - values[i - 1]
+            minimum = min(minimum, difference)
+        return minimum
