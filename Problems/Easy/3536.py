bf457a9d63db8c7fc6ac149458869df373cb008e
MoonesMobaraki <moones.mobaraki@m31.com>
2026-07-27 09:19:57 +0200

    Easy

---
+++ Problems/Easy/3536.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProduct(self, n: int) -> int:
+        digits = [int(d) for d in str(n)]
+        digits.sort(reverse=True)
+        return digits[0] * digits[1]
+    
+    
+my_solution = Solution()
+print(my_solution.maxProduct(n = 31))   #Output: 3
+print(my_solution.maxProduct(n = 124))   #Output: 8
